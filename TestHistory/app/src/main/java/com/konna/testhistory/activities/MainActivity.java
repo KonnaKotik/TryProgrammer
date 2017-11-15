3fe238c9a13ecb3683a0d0535716320a4dd65224
olejiksin <olejiksin@mail.ru>
2017-11-15 10:31:48 +0300

    nemnogo update

--- TestHistory/app/src/main/java/com/konna/testhistory/activities/MainActivity.java
+++ TestHistory/app/src/main/java/com/konna/testhistory/activities/MainActivity.java
@@ -1,14 +1,17 @@
 package com.konna.testhistory.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.konna.testhistory.R;
 import com.konna.testhistory.objects.User;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button goButton;
     User user;
 
     @Override
@@ -21,5 +24,17 @@ public class MainActivity extends AppCompatActivity {
         user = new User(this, username);
 
         Toast.makeText(this, user.getUsername(), Toast.LENGTH_SHORT).show();
+
+
+        goButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TestActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+
+
+
