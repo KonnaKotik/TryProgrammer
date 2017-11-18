ce4ab0f811ada7be1a5b57089fc574d81d3452fb
olejiksin <olejiksin@mail.ru>
2017-11-18 12:48:04 +0300

    nemnogo perexodov

--- TestHistory/app/src/main/java/com/konna/testhistory/activities/MainActivity.java
+++ TestHistory/app/src/main/java/com/konna/testhistory/activities/MainActivity.java
@@ -5,13 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TabHost;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.konna.testhistory.R;
 import com.konna.testhistory.objects.User;
 
 public class MainActivity extends AppCompatActivity {
-    Button goButton;
+    TextView textView;
+    Button ageOneButton;
+    Button ageTwoButton;
+    Button ageThreeButton;
+    Button ageFourButton;
     User user;
 
     @Override
@@ -26,13 +32,40 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, user.getUsername(), Toast.LENGTH_SHORT).show();
 
 
-        goButton.setOnClickListener(new View.OnClickListener() {
+        textView = (TextView) findViewById(R.id.textView2);
+        ageOneButton = findViewById(R.id.ageOneButton);
+        ageTwoButton = findViewById(R.id.ageTwoButton);
+        ageThreeButton = findViewById(R.id.ageThreeButton);
+        ageFourButton = findViewById(R.id.ageFourButton);
+
+        View.OnClickListener OnClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, TestActivity.class);
-                startActivity(intent);
+                Intent intenT1 = new Intent(MainActivity.this, TestActivity1.class);
+                Intent intenT2 = new Intent(MainActivity.this, TestActivity2.class);
+                Intent intenT3 = new Intent(MainActivity.this, TestActivity3.class);
+                Intent intenT4 = new Intent(MainActivity.this, TestActivity4.class);
+                switch (view.getId()) {
+                    case R.id.ageOneButton:
+                        startActivity(intenT1);
+                        break;
+                    case R.id.ageTwoButton:
+                        startActivity(intenT2);
+                        break;
+                    case R.id.ageThreeButton:
+                        startActivity(intenT3);
+                        break;
+                    case R.id.ageFourButton:
+                        startActivity(intenT4);
+                        break;
+
+                }
             }
-        });
+        };
+        ageOneButton.setOnClickListener(OnClickListener);
+        ageTwoButton.setOnClickListener(OnClickListener);
+        ageThreeButton.setOnClickListener(OnClickListener);
+        ageFourButton.setOnClickListener(OnClickListener);
     }
 }
 
---
+++ TestHistory/app/src/main/java/com/konna/testhistory/activities/TestActivity4.java
@@ -0,0 +1,20 @@
+package com.konna.testhistory.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.konna.testhistory.R;
+
+/**
+ * Created by Oleg on 17.11.2017.
+ */
+
+public class TestActivity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac_test4);
+    }
+}
